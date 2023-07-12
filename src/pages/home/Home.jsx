d49133681021832dc901318197f4fd3582ecfe5f
cloud-lab-pvt <areeba@cloudlabweb.com>
2023-07-12 12:26:48 +0500

    changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,8 +3,6 @@ import Navbar from "../../components/navbar/Navbar";
 import "./home.scss";
 import Widget from "../../components/widget/Widget";
 
-import  PieChart  from "../../components/chart/PieChart";
-
 const Home = () => {
   return (
     <div className="home">
@@ -16,17 +14,15 @@ const Home = () => {
           <Widget type="user" />
           <Widget type="devices" />
         </div>
-        
-          <div className="charts">
-        
-        <PieChart />
+
+        <div className="charts">
           {/* <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} /> */}
         </div>
         {/* <div className="listContainer">
           <div className="listTitle">Latest Transactions</div>
            <Table />
            </div> */}
-        
+
         {/* <BarChart /> */}
       </div>
     </div>
